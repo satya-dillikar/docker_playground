56192f8c152b073fa45a945d4d2291e4800e2752
Satya Dillikar <satya.dillikar@gmail.com>
2022-03-12 10:42:00 -0800

    dockerfile update

---
+++ python-docker/server/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+server = Flask(__name__)
+
+@server.route("/")
+def hello():
+    return "Hello World!"
+
+@server.route("/hi")
+def hi():
+    return "Hi!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    server.run(debug=True,host='0.0.0.0',port=port)
